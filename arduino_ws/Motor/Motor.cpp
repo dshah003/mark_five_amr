c3e233f25ae5a0f34cbf19186bf0099befa22258
Darshan Shah <dshah003@terpmail.umd.edu>
2024-08-04 20:48:51 -0400

    Added classes for Motor and PID controllers

---
+++ arduino_ws/Motor/Motor.cpp
@@ -0,0 +1,48 @@
+#include "Motor.h"
+
+Motor::Motor(int motorPin1, int motorPin2, 
+        int pwmPin, int encoderPinA, int encoderPinB) 
+{
+    _motorPin1 = motorPin1;
+    _motorPin2 = motorPin2;
+    _pwmPin = pwmPin;
+    _encoderPinA = encoderPinA;
+    _encoderPinB = encoderPinB;
+    setupPins();
+}
+
+void Motor::setupPins() {
+    pinMode(_motorPin1, OUTPUT);
+    pinMode(_motorPin2, OUTPUT);
+    pinMode(_pwmPin, OUTPUT);
+    pinMode(_encoderPinA, INPUT);
+    pinMode(_encoderPinB, INPUT);
+
+    attachInterrupt(_encoderPinA, readEncoder, RISING);
+}
+
+int Motor::getEncoderAValue() {
+    return digitalRead(_encoderPinA);
+}
+
+int Motor::getEncoderBValue() {
+    int value = digitalRead(_encoderPinB);
+}
+
+void Motor::setMotor(int direction, int pwmValue) {
+    analogWrite(_pwmPin, pwmValue);
+    if (direction == 1) {
+        digitalWrite(_motorPin1, HIGH);
+        digitalWrite(_motorPin2, LOW);
+    } else if (direction == -1) {
+        digitalWrite(_motorPin1, LOW);
+        digitalWrite(_motorPin2, HIGH);
+    } else {
+        digitalWrite(_motorPin1, LOW);
+        digitalWrite(_motorPin2, LOW);
+    }
+}
+
+void Motor::stop() {
+    setMotor(0, 0);
+}
---
+++ arduino_ws/Motor/Motor.h
@@ -0,0 +1,62 @@
+#ifndef Motor_h
+#define Motor_h
+
+#include "Arduino.h"
+
+class Motor {
+    public:
+        /**
+         * Constructs a Motor object with the given pin configurations.
+         *
+         * @param motorPin1 pin number for the first motor input pin
+         * @param motorPin2 pin number for the second motor input pin
+         * @param enablePin pin number for the enable pin
+         * @param encoderPinA pin number for Encoder A pin
+         * @param encoderPinB pin number for Encoder B pin
+         */
+        Motor(int motorPin1, int motorPin2, 
+            int enablePin, int encoderPinA, int encoderPinB);
+        
+        /**
+         * @brief Actuates the Motor to set the speed and direction.
+         * 
+         * @param direction 
+         * @param pwm 
+         */
+        void setMotor(int direction, int pwm);
+        
+        /**
+         * @brief Stop the motors.
+         * 
+         */
+        void stop();
+
+        /**
+         * @brief Get the Encoder's Pin A Value
+         * 
+         * @return int 
+         */
+        int getEncoderAValue();
+
+        /**
+         * @brief Get the Encoder's Pin B Value
+         * 
+         * @return int 
+         */
+        int getEncoderBValue();
+
+    private:
+        int _motorPin1;
+        int _motorPin2;
+        int _enablePin;
+        int _encoderPinA;
+        int _encoderPinB;
+
+        /**
+         * @brief Sets up the pins for the motor appropriately.
+         */
+        void setupPins();
+ 
+};
+
+#endif
