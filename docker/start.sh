e6709806a59833e5b8fbc78c29f17b02def4fc45
Darshan Shah <36118232+dshah003@users.noreply.github.com>
2025-12-29 17:29:16 -0500

    added domain ID to docker run

--- docker/start.sh
+++ docker/start.sh
@@ -56,6 +56,7 @@ docker run \
     --env="XAUTHORITY=/root/.Xauthority" \
     --privileged \
     --network=host \
+    --env=ROS_DOMAIN_ID=0 \
     ${ARDUINO_DEV} \
     ${JOYSTICK_DEV} \
     -v /dev/bus/usb:/dev/bus/usb \
